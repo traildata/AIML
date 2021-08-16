41e55e2020413adf3937e199480a93a68b033c69
Jsingh <jsingh@aaacorp.com>
2021-08-16 17:14:21 +0530

    class addition --------

---
+++ Statistics/stat_base.py
@@ -0,0 +1,31 @@
+
+
+#Convention - Use capital letter to start the name of class name
+
+class Stat_base:
+    """
+    Class description 
+    Base class for Statistics methods 
+
+    """
+
+    def __init__(self): 
+        """
+        Method description : This is called when we create a new instance of the class
+        These are called double unders or dunders
+
+        """
+        pass
+
+    
+
+    def calc_mean():
+
+        pass
+
+    def calc_std():
+        pass
+    
+    def calc_median():
+
+        pass
